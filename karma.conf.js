6feb92a11cb984db02511cd42186bf20f527ffbe
Timur Khatypov <t.khatypov@gmail.com>
2017-01-05 21:13:51 +0300

    Add automatic test

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome'],
+    /*browsers: ['ChromeNoSandbox'],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },*/
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.test.jsx'],
+    preprocessors: {
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client:{
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer:{
+      noInfo: true
+    }
+  });
+};
